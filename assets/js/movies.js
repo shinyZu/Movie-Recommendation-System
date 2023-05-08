535b96003ccc6acfdca296efb4a6601d199a251b
shinyZu <shinysirichandra1141@gmail.com>
2023-05-08 23:26:18 +0530

    created movies page and implemented the logic to load recommended movies"

---
+++ assets/js/movies.js
@@ -0,0 +1,211 @@
+console.log("movies......");
+
+let baseURL = "http://localhost:5000";
+
+const movie_container = $("#card_main_container");
+const dropDown = $("#cmb_movies");
+
+let movieTitles = [];
+let recommendedMovies = [];
+let recommendedMoviePosters = [];
+var selectedOption;
+let cardsData = [];
+
+let img_size = "450x500";
+
+// Sample card data
+// const cardsData = [
+//   {
+//     imgSrc: "https://via.placeholder.com/" + img_size,
+//     title: "Card 1",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     caption: "Caption goes here",
+//   },
+//   {
+//     imgSrc: "https://via.placeholder.com/" + img_size,
+//     title: "Card 2",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     caption: "Caption goes here",
+//   },
+//   {
+//     imgSrc: "https://via.placeholder.com/" + img_size,
+//     title: "Card 3",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     caption: "Caption goes here",
+//   },
+//   {
+//     imgSrc: "https://via.placeholder.com/" + img_size,
+//     title: "Card 4",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     caption: "Caption goes here",
+//   },
+//   {
+//     imgSrc: "https://via.placeholder.com/" + img_size,
+//     title: "Card 5",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     caption: "Caption goes here",
+//   },
+//   {
+//     imgSrc: "https://via.placeholder.com/" + img_size,
+//     title: "Card 6",
+//     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     caption: "Caption goes here",
+//   },
+// ];
+
+// ------
+
+// (function () {
+
+// })();
+
+// Load drop down list
+
+loadMovieTitlesForCmb();
+generateCards("Avatar");
+
+// Function to generate and append cards to the DOM
+function generateCards(movie_title) {
+  const cardContainer = document.getElementById("card_main_container");
+  getRecommendations(movie_title);
+  console.log(cardsData);
+
+  let currentRow = null;
+
+  console.log(cardContainer.childElementCount);
+  if (cardContainer.childElementCount != 0) {
+    console.log("removing children");
+    // cardContainer.removeChild(cardContainer.childNodes);
+    cardContainer.innerHTML = "";
+  }
+
+  // Loop through the card data and generate card elements
+  cardsData.forEach((cardData, index) => {
+    // Create a new row every 3 cards
+    if (index % 3 === 0) {
+      currentRow = document.createElement("div");
+      currentRow.classList.add("row");
+      cardContainer.appendChild(currentRow);
+    }
+
+    const cardCol = document.createElement("div");
+    cardCol.classList.add("col-md-4", "mb-4");
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const img = document.createElement("img");
+    img.classList.add("card-img-top");
+    img.src = cardData.imgSrc;
+    img.alt = cardData.title;
+    card.appendChild(img);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = cardData.title;
+    cardBody.appendChild(cardTitle);
+
+    card.appendChild(cardBody);
+
+    cardCol.appendChild(card);
+    currentRow.appendChild(cardCol);
+  });
+}
+
+function getRecommendations(movie_selected) {
+  $.ajax({
+    url: baseURL + "/recommendations/" + movie_selected,
+    method: "GET",
+    async: false,
+    success: function (resp) {
+      console.log(resp);
+
+      if (resp.status === 200) {
+        console.log("success.........");
+        recommendedMovies = resp.movieList;
+        recommendedMoviePosters = resp.posterList;
+
+        // console.log(recommendedMovies);
+        // console.log(recommendedMoviePosters);
+
+        let x = [];
+        for (let i = 0; i < recommendedMovies.length; i++) {
+          let card_obj = {
+            imgSrc: recommendedMoviePosters[i],
+            title: recommendedMovies[i],
+          };
+          //   card_obj.imgSrc = recommendedMoviePosters[i] + "/" + img_size;
+          //   card_obj.title = recommendedMovies[i];
+          x.push(card_obj);
+        }
+        cardsData = x;
+      } else {
+        console.log("error................");
+        alert(resp.message);
+      }
+    },
+    error: function (ob, textStatus, error) {
+      console.log("server side error................");
+      console.log(ob);
+    },
+  });
+}
+
+function loadMovieTitlesForCmb() {
+  $.ajax({
+    url: baseURL + "/movies",
+    method: "GET",
+    // async: false,
+    success: function (resp) {
+      console.log(resp);
+
+      if (resp.status === 200) {
+        console.log("success.........");
+        movieTitles = JSON.parse(resp.titleList);
+
+        // console.log(movieTitles);
+
+        var select = document.getElementById("cmb_movies");
+
+        for (var i = 0; i < movieTitles.length; i++) {
+          var option = document.createElement("option");
+
+          option.value = i;
+          option.textContent = movieTitles[i];
+          select.appendChild(option);
+        }
+      } else {
+        console.log("error................");
+        alert(resp.message);
+      }
+    },
+    error: function (ob, textStatus, error) {
+      console.log("server side error................");
+      console.log(ob);
+    },
+  });
+}
+
+function recomendMovies() {}
+
+$("#cmb_movies").click(function (e) {
+  console.log("selected.......");
+
+  var selectElement = document.getElementById("cmb_movies");
+  var selectedIndex = selectElement.selectedIndex;
+  selectedOption = selectElement.options[selectedIndex];
+  console.log(selectedOption.value);
+});
+
+$("#btn_recommend").click(function (e) {
+  console.log("clicked.......");
+  for (var i = 0; i < movieTitles.length; i++) {
+    if (i == selectedOption.value) {
+      movie_selected = movieTitles[i];
+      generateCards(movie_selected);
+    }
+  }
+});
--- server.py
+++ server.py
@@ -3,6 +3,11 @@ from flask_cors import CORS, cross_origin
 import mysql.connector
 import webbrowser
 
+import pickle
+import pandas as pd
+import requests
+
+
 app = Flask(__name__)
 CORS(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -19,6 +24,51 @@ conn = mysql.connector.connect(
     database=app.config['MYSQL_DB']
 )
 
+login_page_url = "http://127.0.0.1:5000/"
+main_app_url = "http://localhost:8501/"
+
+movies_dict = pickle.load(open('pickle/movie_dict.pkl', 'rb')) # rb = read binary mode
+movies =pd.DataFrame(movies_dict)
+similarity = pickle.load(open('pickle/similarity.pkl', 'rb'))
+
+cmb_movie_list = movies['title'].values
+
+def get_movie_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=c3ed154bde1307169e62092fa886bb9c&language=en-US'.format(movie_id))
+    data = response.json()
+    # print(data)
+    # st.text(data)
+    poster_url = poster_path_base_url + data['poster_path']
+    # st.text(poster_url)
+    return poster_url
+
+def recommend_movies(movie_selected):
+    movie_index = movies[movies['title'] == movie_selected].index[0]
+    # st.text(movie_index)
+    distances = similarity[movie_index]
+    # st.text(distances)
+    movies_list = sorted(list(enumerate(distances)), reverse = True, key=lambda x:x[1])[1:21]
+    # st.text(movies_list)
+
+    recommended_movies_list = []
+    recommended_movies_poster_list = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        # st.text(movie_id)
+        recommended_movies_list.append(movies.iloc[i[0]].title)
+        # st.text(recommended_movies_list)
+
+        # fetch poster from API
+        recommended_movies_poster_list.append(get_movie_poster(movie_id))
+        # st.text(len(recommended_movies_list))
+    return recommended_movies_list, recommended_movies_poster_list
+
+
+API_KEY = "c3ed154bde1307169e62092fa886bb9c"
+poster_path_base_url = "https://image.tmdb.org/t/p/original"
+
+
 @app.route('/')
 @cross_origin()
 def index():
@@ -27,6 +77,27 @@ def index():
     mycursor.execute("DROP TABLE IF EXISTS customers")
     mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), address VARCHAR(255), contact VARCHAR(10))")
     return render_template('server_index.html')
+
+@app.route('/movies', methods=["GET"])
+@cross_origin()
+def getMovieTitles(): 
+    # Convert the ndarray to a list
+    x = cmb_movie_list.tolist()
+
+    # Serialize the list using JSON
+    y = json.dumps(x)
+
+    # print(cmb_movie_list)
+    return jsonify({'status': 200, 'titleList': y})
+
+@app.route('/recommendations/<string:movie_selected>', methods=["GET"])
+@cross_origin()
+def getRecommendedMovies(movie_selected):
+    print(movie_selected)
+    titles,posters = recommend_movies(movie_selected)
+    return jsonify({'status': 200, 'movieList': titles, 'posterList': posters })
+    # return recommended_movies_list, recommended_movies_poster_list
+
 
 @app.route('/save', methods=["POST"])
 @cross_origin()
